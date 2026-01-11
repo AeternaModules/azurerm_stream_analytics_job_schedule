4ab8fc9c66ed05717b69341f7f564d38196228d9
Daniel Allen <dan@aeternagroup.net>
2026-01-11 19:32:09 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "stream_analytics_job_schedules" {
+  description = "All stream_analytics_job_schedule resources"
+  value       = azurerm_stream_analytics_job_schedule.stream_analytics_job_schedules
+}
+
