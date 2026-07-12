a8efea73ef4be4d229d89c6abb32f8d51c8ab1f7
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:55 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "stream_analytics_job_schedules" {
-  description = "All stream_analytics_job_schedule resources"
-  value       = azurerm_stream_analytics_job_schedule.stream_analytics_job_schedules
-}
 output "stream_analytics_job_schedules_last_output_time" {
-  description = "List of last_output_time values across all stream_analytics_job_schedules"
-  value       = [for k, v in azurerm_stream_analytics_job_schedule.stream_analytics_job_schedules : v.last_output_time]
+  description = "Map of last_output_time values across all stream_analytics_job_schedules, keyed the same as var.stream_analytics_job_schedules"
+  value       = { for k, v in azurerm_stream_analytics_job_schedule.stream_analytics_job_schedules : k => v.last_output_time }
 }
 output "stream_analytics_job_schedules_start_mode" {
-  description = "List of start_mode values across all stream_analytics_job_schedules"
-  value       = [for k, v in azurerm_stream_analytics_job_schedule.stream_analytics_job_schedules : v.start_mode]
+  description = "Map of start_mode values across all stream_analytics_job_schedules, keyed the same as var.stream_analytics_job_schedules"
+  value       = { for k, v in azurerm_stream_analytics_job_schedule.stream_analytics_job_schedules : k => v.start_mode }
 }
 output "stream_analytics_job_schedules_start_time" {
-  description = "List of start_time values across all stream_analytics_job_schedules"
-  value       = [for k, v in azurerm_stream_analytics_job_schedule.stream_analytics_job_schedules : v.start_time]
+  description = "Map of start_time values across all stream_analytics_job_schedules, keyed the same as var.stream_analytics_job_schedules"
+  value       = { for k, v in azurerm_stream_analytics_job_schedule.stream_analytics_job_schedules : k => v.start_time }
 }
 output "stream_analytics_job_schedules_stream_analytics_job_id" {
-  description = "List of stream_analytics_job_id values across all stream_analytics_job_schedules"
-  value       = [for k, v in azurerm_stream_analytics_job_schedule.stream_analytics_job_schedules : v.stream_analytics_job_id]
+  description = "Map of stream_analytics_job_id values across all stream_analytics_job_schedules, keyed the same as var.stream_analytics_job_schedules"
+  value       = { for k, v in azurerm_stream_analytics_job_schedule.stream_analytics_job_schedules : k => v.stream_analytics_job_id }
 }
 
