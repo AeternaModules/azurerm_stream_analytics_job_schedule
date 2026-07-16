6c4de6454e4e52c8457aa75de76fc7141f3f19d0
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:51:27 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 output "stream_analytics_job_schedules_id" {
   description = "Map of id values across all stream_analytics_job_schedules, keyed the same as var.stream_analytics_job_schedules"
-  value       = { for k, v in azurerm_stream_analytics_job_schedule.stream_analytics_job_schedules : k => v.id }
+  value       = { for k, v in azurerm_stream_analytics_job_schedule.stream_analytics_job_schedules : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "stream_analytics_job_schedules_last_output_time" {
   description = "Map of last_output_time values across all stream_analytics_job_schedules, keyed the same as var.stream_analytics_job_schedules"
-  value       = { for k, v in azurerm_stream_analytics_job_schedule.stream_analytics_job_schedules : k => v.last_output_time }
+  value       = { for k, v in azurerm_stream_analytics_job_schedule.stream_analytics_job_schedules : k => v.last_output_time if v.last_output_time != null && length(v.last_output_time) > 0 }
 }
 output "stream_analytics_job_schedules_start_mode" {
   description = "Map of start_mode values across all stream_analytics_job_schedules, keyed the same as var.stream_analytics_job_schedules"
-  value       = { for k, v in azurerm_stream_analytics_job_schedule.stream_analytics_job_schedules : k => v.start_mode }
+  value       = { for k, v in azurerm_stream_analytics_job_schedule.stream_analytics_job_schedules : k => v.start_mode if v.start_mode != null && length(v.start_mode) > 0 }
 }
 output "stream_analytics_job_schedules_start_time" {
   description = "Map of start_time values across all stream_analytics_job_schedules, keyed the same as var.stream_analytics_job_schedules"
-  value       = { for k, v in azurerm_stream_analytics_job_schedule.stream_analytics_job_schedules : k => v.start_time }
+  value       = { for k, v in azurerm_stream_analytics_job_schedule.stream_analytics_job_schedules : k => v.start_time if v.start_time != null && length(v.start_time) > 0 }
 }
 output "stream_analytics_job_schedules_stream_analytics_job_id" {
   description = "Map of stream_analytics_job_id values across all stream_analytics_job_schedules, keyed the same as var.stream_analytics_job_schedules"
-  value       = { for k, v in azurerm_stream_analytics_job_schedule.stream_analytics_job_schedules : k => v.stream_analytics_job_id }
+  value       = { for k, v in azurerm_stream_analytics_job_schedule.stream_analytics_job_schedules : k => v.stream_analytics_job_id if v.stream_analytics_job_id != null && length(v.stream_analytics_job_id) > 0 }
 }
 
